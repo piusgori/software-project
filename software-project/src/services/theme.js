4fed2962f3cb2a7b4ff8de5e76392b269c3b4b4f
piusgori <piusgori@gmail.com>
2023-02-18 10:30:53 +0300

    Switch client to mobile from web

--- software-project/src/services/theme.js
+++
@@ -1,8 +0,0 @@
-import { createTheme } from '@mui/material';
-const font = "'Old Standard TT', serif";
-
-export const theme = createTheme({
-    typography: { 
-        fontFamily: font,
-     }
-})
