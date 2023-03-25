1029d3fd83d2d98ffac457748fa928bf7f1312fb
piusgori <piusgori@gmail.com>
2023-03-25 18:37:43 +0300

    Complete auth

--- client/src/pages/auth/LoginPage.jsx
+++ client/src/pages/auth/LoginPage.jsx
@@ -1,11 +1,13 @@
-import React, { useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { styled } from '@mui/material/styles';
-import { Box, Button, Card, Typography } from '@mui/material';
+import { Alert, Box, Button, Card, CircularProgress, Snackbar, Typography } from '@mui/material';
 import { useForm } from '../../hooks/useForm';
 import { GitHub } from '@mui/icons-material';
 import { useNavigate } from 'react-router-dom';
 import { GITHUB_CLIENT_ID } from '../../utils/firebase';
 import { CLIENT_URL, SERVER_URL } from '../../utils/data';
+import { AuthContext } from '../../services/auth-context';
+import axios from 'axios';
 
 const MainBox = styled(Box)(() => ({
   height: '100vh',
@@ -40,6 +42,7 @@ const FormButton = styled(Button)(() => ({
 const LoginPage = () => {
 
   const navigate = useNavigate();
+  const { login } = useContext(AuthContext);
   const loginForm = [
     { name: 'email', label: 'Email', type: 'email', placeholder: 'Your Email Address' },
     { name: 'password', label: 'Password', type: 'password', placeholder: 'Your Password' },
@@ -65,17 +68,16 @@ const LoginPage = () => {
       if (!code) return;
       if (code) {
         setIsLoading(true);
-        const { data } = await axios.get(`${SERVER_URL}/auth/access-token/${code}`);
+        const { data } = await axios.get(`${SERVER_URL}/auth/access-token?code=${code}&path=login`);
         const splitData = data.split('=');
         if(splitData[0] === 'access_token') {
           const at = splitData[1].split('&scope')[0];
           if(at) {
             const config = { headers: { 'Authorization': `Bearer ${at}` } }
             const res = await axios.get(`https://api.github.com/user`, config);
-            const { id, name, email, login } = res.data;
-            const splitNames = name.split(' ');
-            const githubForm = { email: email || `${login}@gmail.com`, firstName: splitNames[0], lastName: splitNames[1], githubId: id };
-            await signupHandler(githubForm);
+            const { id } = res.data;
+            const githubForm = { githubId: id };
+            await loginHandler(githubForm);
           } else {
             setIsLoading(false);
           }
@@ -86,22 +88,60 @@ const LoginPage = () => {
     }
   };
 
+  const loginHandler = async (form) => {
+    try {
+      setIsLoading(true);
+      setSuccess(false);
+      setError(false);
+      setErrorMessage('');
+      await login(form);
+      setSuccess(true);
+      setTimeout(() => {
+        setSuccess(false);
+        navigate('/home');
+      }, 3000);
+    } catch (err) {
+      const errMessage = err?.response?.data?.content || err?.response?.data?.message || err?.response?.message || err?.message;
+      setSuccess(false);
+      setError(true);
+      setErrorMessage(errMessage);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    const isBDis = (!data.email.includes('@') || data.password.length < 1);
+    setButtonDisabled(isBDis);
+  }, [data]);
+
+  useEffect(() => {
+    handleGitHubSignInCallback();
+  }, []);
+
   return (
     <MainBox>
       <AuthCard>
         <Typography color='primary' variant='h3'>Dev Hub</Typography>
         <Typography sx={{ color: '#515151', mb: 5 }} variant='h6'>Login To Your Account</Typography>
         {Inputs}
-        <>
-          <FormButton variant='contained' disableElevation>Login</FormButton>
+        {isLoading && <CircularProgress />}
+        {!isLoading && <>
+          <FormButton disabled={buttonDisabled} onClick={() => loginHandler(data)} variant='contained' disableElevation>Login</FormButton>
           <Typography sx={{ color: '#515151' }} variant='h6'>Or</Typography>
-          <Button startIcon={<GitHub />} variant='outlined'>Login With GitHub</Button>
+          <Button onClick={handleGitHubSignIn} startIcon={<GitHub />} variant='outlined'>Login With GitHub</Button>
           <BelowContainer>
             <Typography sx={{ color: '#515151' }}>Don't Have An Account</Typography>
             <Button onClick={() => { navigate('/signup') }} variant='text'> Sign Up</Button>
           </BelowContainer>
-        </>
+        </>}
       </AuthCard>
+      <Snackbar anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }} open={success} autoHideDuration={6000} onClose={() => { setSuccess(false) }}>
+          <Alert onClose={() => { setSuccess(false) }} severity='success' sx={{ width: '100%' }}>You have logged in successfully</Alert>
+      </Snackbar>
+      <Snackbar anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }} open={error} autoHideDuration={10000} onClose={() => { setError(false) }}>
+          <Alert onClose={() => { setError(false) }} severity='error' sx={{ width: '100%' }}>{errorMessage}</Alert>
+      </Snackbar>
     </MainBox>
   )
 }
